a8dd2d683ce322b4806f0097c2a71d996f901f7f
EmerArte <48594349+EmerArte@users.noreply.github.com>
2020-05-24 12:26:59 -0500

    LISTO PARA SEGUIR PORGRAMANDO

---
+++ app/src/main/java/com/example/cecyapp/olvidarPass.java
@@ -0,0 +1,16 @@
+package com.example.cecyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cecyapp.R;
+
+public class olvidarPass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olvidar_pass);
+    }
+}
---
+++ app/src/main/java/com/example/cecyapp/usuario_Home.java
@@ -0,0 +1,15 @@
+package com.example.cecyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.cecyapp.R;
+
+public class usuario_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario__home);
+    }
+}
